fc5790aef927a90aeea37a6e3118173456b46290
Prathamesh Desai <115919176+pratham7512@users.noreply.github.com>
2024-04-26 14:09:11 +0530

    Delete routes directory

--- routes/response.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const router=express.Router();
-
-const {responseGenerator,geminiResponse}=require("../controllers/response")
-
-
-router.post("/",responseGenerator);
-router.post("/gemini",geminiResponse);
-module.exports=router;
